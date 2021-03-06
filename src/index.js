f77f2678831b59cd8b0ff6082d4786782208846d
eugenemp <phoenixlp91@mail.ru>
2021-03-07 02:24:33 +0300

    feat: implement check() function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  //Make an array of config chars concatenated into pairs.
+  //it will made next steps easier
+  let config = [];
+  
+  bracketsConfig.forEach(element => {
+    config.push(element.join(''))
+  });
+
+  //Define function that replaces config brackets if it find ones with emptiness 
+  function findBrackets(str) {
+    //Save input string length. Once the function finish the job 
+    //it will compare output length with input.
+    let lengthBefore = str.length;
+
+    config.forEach(element => {
+      str = str.replace(element, '');
+    });
+    //Same length mean no changes are made so return string as is...
+    if(str.length === lengthBefore) {
+      return str;
+      //...or if chages are made we need to start the process over again
+      //by recursively calling findBrackets function
+    } else return findBrackets(str);
+  }
+  //Return true if output string is empty or false if it is not
+  return findBrackets(str).length === 0;
 }
